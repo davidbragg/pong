5d39100abf7d5bc55f194ded6d4a7b28025234fe
David Bragg <dshute@gmail.com>
2024-02-23 16:06:30 -0500

    run from vscode working, added ball, boundaries, and scoring mechanism

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,12 +1,16 @@
 extends RigidBody2D
 
-var speed = 400
-var angle = deg_to_rad(45)
+@export var speed = 800
+# 0 right | 90 up | 180 left | 270 down
+# clamp somewhere around 15 degrees off each?
+# 15 - 75 | 105 - 165 | 195 - 255 | 285 - 345
+var degrees = 45
+var angle = deg_to_rad(degrees)
 var x_direction = cos(angle)
 var y_direction = -sin(angle)
 var velocity
 
-
+var score = [0, 0]
 
 func _ready():
 	# set FREEZE_MODE_KINEMATIC to remove gravity and forces
@@ -15,15 +19,27 @@ func _ready():
 	freeze_mode = RigidBody2D.FREEZE_MODE_KINEMATIC
 
 
-
 func _process(delta):
 	velocity = Vector2(x_direction, y_direction) * speed
 	move_and_collide(velocity * delta)
-	print(position.x, " ", position.y)
 
-	if position.y < 60 or position.y > 588:
-		y_direction *= -1
 
-	if position.x < 60 or position.x > 1092:
-		x_direction *= -1
+# Handle Y direction collision
+func _on_area_2d_body_entered(_body:Node2D):
+	y_direction *= -1
 
+
+func _on_bottom_boundary_body_entered(_body:Node2D):
+	y_direction *= -1
+
+
+#Handle X direction collision
+func _on_right_boundary_body_entered(body:Node2D):
+	x_direction *= -1
+	score[0] += 1
+	print(score)
+
+func _on_left_boundary_body_entered(body:Node2D):
+	x_direction *= -1
+	score[1] += 1
+	print(score)
