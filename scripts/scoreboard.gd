b5d6bc49731ea61eaa5fe1441eba97548df8fc25
David Bragg <dshute@gmail.com>
2024-02-25 17:45:53 -0500

    code clean up

--- scripts/scoreboard.gd
+++ scripts/scoreboard.gd
@@ -1,20 +1,15 @@
 extends Node2D
 
 var score = [0, 0]
-@onready var p1text = %Player1Score
-@onready var p2text = %Player2Score
-@onready var ball = %Ball
 
 func _ready():
-	ball.connect("goal", Callable(self,"_on_goal"))
+	%Ball.connect("goal", Callable(self,"_on_goal"))
 
 func _on_goal(collider):
-	print("collider: ", collider)
 	if collider == "BoundaryLeft":
 		score[1] += 1
 	else:
 		score[0] += 1
 
-	p1text.text = str(score[0])
-	p2text.text = str(score[1])
-
+	%Player1Score.text = str(score[0])
+	%Player2Score.text = str(score[1])
